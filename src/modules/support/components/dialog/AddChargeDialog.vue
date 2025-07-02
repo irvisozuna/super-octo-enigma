1a3cb12477c529d77442c64a324caaf0cf5f8713
irvis isaac ozuna castillo <irvis_isaac@hotmail.com>
2025-07-01 22:07:15 -0700

    Add user ID handling in AddChargeDialog component: retrieve aquasoft_id from cookies, validate its presence before form submission, and include it in the payload for charge creation to ensure proper user association.

--- src/modules/support/components/dialog/AddChargeDialog.vue
+++ src/modules/support/components/dialog/AddChargeDialog.vue
@@ -8,6 +8,7 @@ const { showSuccess, showError } = useNotification()
 const contractStore = useContractStore()
 const { closeDialog } = useAppManager()
 const isLoading = ref(false)
+const { aquasoft_id, name } = useCookie('userData').value
 
 // Validation and Form
 const { handleSubmit, errors } = useForm({
@@ -84,11 +85,17 @@ const onFormSubmit = async () => {
 
   try {
     isLoading.value = true
+    if (!aquasoft_id) {
+      showError(t('error_creating_note'))
+
+      return
+    }
 
     const payload = {
       charges: addedCharges.value,
       account: contractStore.item.account,
       token: token_dolibarr,
+      user_id: aquasoft_id,
     }
 
     const response = await contractStore.createItem(payload, '/charges')
